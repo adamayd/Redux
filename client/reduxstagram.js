a084ff94d771ca7f2908a381668fb3fba3dee369
Adam Ayd <adamayd@gmail.com>
2018-03-10 21:22:05 -0500

    create base components

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,13 @@
-// let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// Import the css
+import css from './styles/style.styl';
+
+// Import Components
+import Main from './components/Main';
+
+render(<Main />, document.getElementById('root'));
+
+

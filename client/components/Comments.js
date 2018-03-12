55d693a5b666d6c444bb3a51a5a4a7da27a8eabc
Adam Ayd <adamayd@gmail.com>
2018-03-11 23:41:04 -0400

    created comment state and passed to store

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -12,10 +12,28 @@ class Comments extends React.Component {
       </div>
     )
   }
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('submitting the form')
+    const { postId } = this.props.params;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    console.log(postId, author, comment);
+    this.props.addComment(postId, author, comment)
+  }
   render() {
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
+        <form 
+          ref="commentForm" 
+          className="comment-form" 
+          onSubmit={(event) => this.handleSubmit(event)}
+        >
+          <input type="text" ref="author" placeholder="author" />
+          <input type="text" ref="comment" placeholder="comment" />
+          <input type="submit" hidden />
+        </form>
       </div>
     )
   }

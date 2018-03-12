35cb6004447ba7beb7d1371df59f7c592a56e3e4
Adam Ayd <adamayd@gmail.com>
2018-03-11 23:13:44 -0400

    added comment section to single component

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 class Comments extends React.Component {
+  renderComment(comment, index) {
+    return (
+      <div className="comment" key={index}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    )
+  }
   render() {
     return (
-      <div className="comment">
-        I'm the comments!!!
+      <div className="comments">
+        {this.props.postComments.map(this.renderComment)}
       </div>
     )
   }

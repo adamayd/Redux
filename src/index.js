1503636f60e9eca6c35bc5dccae0f40303ca978c
Adam Ayd <adam.ayd@gmail.com>
2018-03-22 00:57:28 -0400

    added middleware to store

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
 import C from './constants'
-import appReducer from './store/reducers'
-// import initialState from './initialState.json'
-import { createStore } from 'redux'
+import storeFactory from './store'
 
-const store = createStore(appReducer)
+const initialState = (localStorage['redux-store']) ?
+  JSON.parse(localStorage['redux-store']) :
+  {}
 
-const unsubscribeGoalLogger = store.subscribe(() => console.log(`   Goal: ${store.getState().goal}`))
+const saveState = () => {
+  const state = JSON.stringify(store.getState())
+  localStorage['redux-store'] = state
+}
 
-setInterval(() => {
-  store.dispatch({
-    type: C.SET_GOAL,
-    payload: Math.floor(Math.random() * 100)
-  })
-}, 250)
+const store = storeFactory(initialState)
 
-setTimeout(() => {
-  unsubscribeGoalLogger();
-}, 3000)
+store.subscribe(saveState)
+
+store.dispatch({
+  type: C.ADD_DAY,
+  payload: {
+    "resort": "Mt Shasta",
+    "date": "2016-10-28",
+    "powder": true,
+    "backcountry": true
+  }
+})
+store.dispatch({
+  type: C.ADD_DAY,
+  payload: {
+    "resort": "Squaw Valley",
+    "date": "2016-3-28",
+    "powder": true,
+    "backcountry": false
+  }
+})
+store.dispatch({
+  type: C.ADD_DAY,
+  payload: {
+    "resort": "Mt Shasta",
+    "date": "2016-1-28",
+    "powder": false,
+    "backcountry": true
+  }
+})

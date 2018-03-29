8a6f52656e4cd1e3b5a16271a96c3d32e39a8283
Adam Ayd <adam.ayd@gmail.com>
2018-03-28 23:55:22 -0400

    had to copy and re-npm install, downgraded react :(

--- src/actions.js
+++ src/actions.js
@@ -3,84 +3,77 @@ import fetch from 'isomorphic-fetch'
 
 export function addDay(resort, date, powder=false, backcountry=false) {
 
-  return {
-    type: C.ADD_DAY,
-    payload: {resort, date, powder, backcountry}
-  }
+    return {
+        type: C.ADD_DAY,
+        payload: {resort,date,powder,backcountry}
+    }
+
 }
 
 export const removeDay = function(date) {
-  
-  return {
-    type: C.REMOVE_DAY,
-    payload: date
-  }
+
+    return {
+        type: C.REMOVE_DAY,
+        payload: date
+    }
+
 }
 
-export const setGoal = (goal) =>
-  ({
-    type: C.SET_GOAL,
-    payload: goal 
-  })
+export const setGoal = (goal) => 
+    ({
+        type: C.SET_GOAL,
+        payload: goal
+    })
 
-export const addError = (err) =>
-  ({
-    type: C.ADD_ERROR,
-    payload: err
-  })
+export const addError = (message) => 
+   ({
+      type: C.ADD_ERROR,
+      payload: message
+   })
 
-export const clearError = (idx) =>
-  ({
-    type: C.CLEAR_ERROR,
-    payload: idx
-  })
+export const clearError = index => 
+    ({
+        type: C.CLEAR_ERROR,
+        payload: index
+    })   
 
-export const changeSuggestions = (suggs) =>
+export const changeSuggestions = suggestions => 
   ({
     type: C.CHANGE_SUGGESTIONS,
-    payload: suggs
+    payload: suggestions
   })
 
-export const clearSuggestions = () =>
-  ({
-    type: C.CLEAR_SUGGESTIONS
-  })
+export const clearSuggestions = () => 
+    ({
+        type: C.CLEAR_SUGGESTIONS
+    })
 
-export const randomGoals = () => (dispatch, getState) => {
+export const suggestResortNames = value => dispatch => {
 
-  if (!getState().resortNames.fetching) {
     dispatch({
-      type: C.FETCH_RESORT_NAMES
+        type: C.FETCH_RESORT_NAMES
     })
-  
-    setTimeout(() => {
-      dispatch({
-        type: C.CANCEL_FETCHING
-      })
-    }, 1500)
-  }
-}
 
-export const suggestResortNames = (value) => (dispatch) => {
+    fetch('http://localhost:3333/resorts/' + value)
+        .then(response => response.json())
+        .then(suggestions => {
 
-  dispatch({
-    type: C.FETCH_RESORT_NAMES
-  })
+            dispatch({
+                type: C.CHANGE_SUGGESTIONS,
+                payload: suggestions
+            })
 
-  fetch('http://localhost:3333/resorts/' + value)
-    .then(response => response.json())
-    .then(suggestions => {
-      dispatch({
-        type: C.CHANGE_SUGGESTIONS,
-        payload: suggestions
-      })
-    })
-    .catch(error => {
-      dispatch(
-        addError(error.message)
-      )
-      dispatch({
-        type: C.CANCEL_FETCHING
-      })
-    })
+        })
+        .catch(error => {
+
+            dispatch(
+                addError(error.message)
+            )
+
+            dispatch({
+                type: C.CANCEL_FETCHING
+            })
+
+        })
+
 }
--- src/index.js
+++ src/index.js
@@ -1,90 +1,19 @@
-import storeFactory from './store'
-// import { 
-//   addDay, 
-//   removeDay, 
-//   setGoal,
-//   addError,
-//   clearError,
-//   changeSuggestions,
-//   clearSuggestions } from './actions'
-// import { randomGoals } from './actions'
-import { suggestResortNames } from './actions'
+import C from './constants'
+import React from 'react'
+import { render } from 'react-dom'
+import routes from './routes'
+import sampleData from './initialState'
 
-const store = storeFactory()
+const initialState = (localStorage["redux-store"]) ?
+    JSON.parse(localStorage["redux-store"]) :
+    sampleData
 
-// store.dispatch(
-//   addDay("Heavenly", "2016-12-22")
-// )
+const saveState = () => 
+    localStorage["redux-store"] = JSON.stringify(store.getState())
 
-// store.dispatch(
-//   removeDay("2016-12-22")
-// )
+window.React = React
 
-// store.dispatch(
-//   setGoal(55)
-// )
-
-// store.dispatch(
-//   addError("something went wrong")
-// )
-
-// store.dispatch(
-//   clearError(0)
-// )
-
-// store.dispatch(
-//   changeSuggestions(['One', 'Two', 'Three'])
-// )
-
-// store.dispatch(
-//   clearSuggestions()
-// )
-
-// store.dispatch(
-//   randomGoals()
-// )
-
-store.dispatch(
-  suggestResortNames("hea")
+render(
+	routes,
+  document.getElementById('react-container')
 )
-
-// const initialState = (localStorage['redux-store']) ?
-//   JSON.parse(localStorage['redux-store']) :
-//   {}
-
-// const saveState = () => {
-//   const state = JSON.stringify(store.getState())
-//   localStorage['redux-store'] = state
-// }
-
-// const store = storeFactory(initialState)
-
-// store.subscribe(saveState)
-
-// store.dispatch({
-//   type: C.ADD_DAY,
-//   payload: {
-//     "resort": "Mt Shasta",
-//     "date": "2016-10-28",
-//     "powder": true,
-//     "backcountry": true
-//   }
-// })
-// store.dispatch({
-//   type: C.ADD_DAY,
-//   payload: {
-//     "resort": "Squaw Valley",
-//     "date": "2016-3-28",
-//     "powder": true,
-//     "backcountry": false
-//   }
-// })
-// store.dispatch({
-//   type: C.ADD_DAY,
-//   payload: {
-//     "resort": "Mt Shasta",
-//     "date": "2016-1-28",
-//     "powder": false,
-//     "backcountry": true
-//   }
-// })
